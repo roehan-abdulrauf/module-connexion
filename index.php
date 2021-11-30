a2307a8fc2723c6954a4a7e199deaa9743504584
roehan-abdulrauf <91737334+roehan-abdulrauf@users.noreply.github.com>
2021-11-30 17:28:32 +0100

    new modif

--- index.php
+++ index.php
@@ -38,48 +38,48 @@
     </header>
     <main>
         <section id="page-index">
-            <div id="textdeb">
-                <div class="prof">
-                    <h1>N’ayez plus peur des travaux </h1>
-                    <div class="centrer-text">
-                        <p class="parede-rea">PAREDE réalise vos travaux de maçonnerie générale et façade. Du gros œuvre au carrelage intérieur, en construction, rénovation ou réparation, nous intervenons rapidement et sur devis gratuit.</p>
-                    </div>
+            <div class="prof">
+                <h1>N’ayez plus peur des travaux </h1>
+                <div class="centrer-text">
+                    <p class="parede-rea">PAREDE réalise vos travaux de maçonnerie générale et façade. Du gros œuvre au carrelage intérieur, en construction, rénovation ou réparation, nous intervenons rapidement et sur devis gratuit.</p>
                 </div>
-                <h2 class="h2-index">Nous réalisons vos travaux avec passion et sérieux</h2>
-                <div class="accueil">
-                    <div>
-                        <img src="https://zupimages.net/up/21/47/eqxn.jpg" alt="" width="80%" />
-                        <h3>Un service clé en main pour des travaux de qualité.</h3>
-                        <p>"Une très bonne expérience du début à la fin. Un grand merci pour votre disponibilité, écoute et professionnalisme."</p>
-                    </div>
-                    <div>
-                        <img src="https://zupimages.net/up/21/47/d0ik.jpg" alt="" width="80%" />
-                        <h3>Idéal pour vous réconcilier avec les travaux !</h3>
-                        <p>"Une aide précieuse et rassurante tout au long du chantier. De plus, le résultat est magnifique !"</p>
-                    </div>
+            </div>
+            <h2 class="h2-index">Nous réalisons vos travaux avec passion et sérieux</h2>
+            <div class="accueil">
+                <div>
+                    <img src="https://zupimages.net/up/21/47/eqxn.jpg" alt="" width="80%" />
+                    <h3>Un service clé en main pour des travaux de qualité.</h3>
+                    <p>"Une très bonne expérience du début à la fin. Un grand merci pour votre disponibilité, écoute et professionnalisme."</p>
                 </div>
-                <h2 class="titre-fin">Confiez vos travaux a PAREDE pour des travaux de qualités </h2>
-                <section id="debut-fin">
-                    <div>
-                        <h4> Analyse et devis</h4><br>
-                        <p>Notre chef de projet analyse vos besoins <br>
-                            avant de vous soumettre un avant-projet<br>
-                            ainsi qu’un devis estimatif pour vos travaux.</p>
-                    </div>
-                    <div>
-                        <h4>Production et plan d’exécution</h4><br>
-                        <p>Nous élaborons les cahiers des charges<br>
-                            pour chaque corps de métier et analysons<br>
-                            minutieusement leurs offres.</p>
-                    </div>
-                    <div>
-                        <h4>Suivi de chantier</h4><br>
-                        <p>Durant toute la durée du chantier,<br>
-                            un rapport de suivi hebdomadaire est<br>
-                            réalisé jusqu’à la réception des travaux.</p>
-                    </div>
-                </section>
-                <h2 class="h2-indexe">PAREDE c'est une équipe de Super experts à vos côtés </h2>
+                <div>
+                    <img src="https://zupimages.net/up/21/47/d0ik.jpg" alt="" width="80%" />
+                    <h3>Idéal pour vous réconcilier avec les travaux !</h3>
+                    <p>"Une aide précieuse et rassurante tout au long du chantier. De plus, le résultat est magnifique !"</p>
+                </div>
+            </div>
+            <h2 class="titre-fin">Confiez vos travaux a PAREDE pour des travaux de qualités </h2>
+            <section id="debut-fin">
+                <div>
+                    <h4> Analyse et devis</h4><br>
+                    <p>Notre chef de projet analyse vos besoins <br>
+                        avant de vous soumettre un avant-projet<br>
+                        ainsi qu’un devis estimatif pour vos travaux.</p>
+                </div>
+                <div>
+                    <h4>Production et plan d’exécution</h4><br>
+                    <p>Nous élaborons les cahiers des charges<br>
+                        pour chaque corps de métier et analysons<br>
+                        minutieusement leurs offres.</p>
+                </div>
+                <div>
+                    <h4>Suivi de chantier</h4><br>
+                    <p>Durant toute la durée du chantier,<br>
+                        un rapport de suivi hebdomadaire est<br>
+                        réalisé jusqu’à la réception des travaux.</p>
+                </div>
+            </section>
+            <h2 class="h2-indexe">PAREDE c'est une équipe de Super experts à vos côtés </h2>
+        </section>
     </main>
     <footer>
         <div class="footer-dark">
